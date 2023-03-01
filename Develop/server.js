a3c75be6bb4ad7ec3fbee1a18975ca3971c5e281
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-03-01 14:52:03 -0800

    fixed htmlRoutes path

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // const path = require('path');
 const api = require('./routes/apiRoutes.js');
-const notes = require('./routes/notesRoutes.js');
+const notes = require('./routes/htmlRoutes.js');
 
 
 
