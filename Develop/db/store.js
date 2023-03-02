92e77a1a748ad77990f2e9db482340343f5e709b
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-03-02 14:28:30 -0800

    added stote.js code

--- Develop/db/store.js
+++ Develop/db/store.js
@@ -1,9 +1,35 @@
 const json = require('./db.json');
 const fs = require('fs');
+const util = require('util');
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
-function getNotes(){
-    return json;
 
-} 
+class Store {
+    read() {
+        return readFileAsync('db/db.json', 'utf8');
+    }
 
-module.exports = getNotes;
+    write(note) {
+        return writeFileAsync('db/db.json', JSON.stringify(note));
+    }
+
+    async getNotes() {
+        const data = await this.read();
+        return JSON.parse(data);
+    }
+
+    async addNote(note) {
+        const notes = await this.getNotes();
+        notes.push(note);
+        await this.write(notes);
+    }
+
+    async removeNote(id) {
+        const notes = await this.getNotes();
+        const filteredNotes = notes.filter(note => note.id !== id);
+        await this.write(filteredNotes);
+    }
+}
+
+module.exports = new Store();
--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -14,7 +14,7 @@ router.get('/notes', (req, res) => {
             return res.json(notes);
         })
         .catch((err) => res.status(500).json(err));
-   
+
 });
 
 router.post('/api/notes', (req, res) => {
@@ -29,52 +29,45 @@ router.post('/api/notes', (req, res) => {
             text,
         }
 
+        // GET "/api/notes" responds with all notes from the database
+        router.get('/notes', (req, res) => {
+            console.log('route hit')
+            store
+                .getNotes()
+                .then((notes) => {
+                    return res.json(notes);
+                })
+                .catch((err) => res.status(500).json(err));
+            res.json(json)
+        });
 
-const router = require('express').Router();
-const store = require('../db/store');
-const json = require('../db/db.json');
-const fs = require('fs');
-
-
-// GET "/api/notes" responds with all notes from the database
-router.get('/notes', (req, res) => {
-    console.log('route hit')
-    store
-        .getNotes()
-        .then((notes) => {
-            return res.json(notes);
-        })
-        .catch((err) => res.status(500).json(err));
-    res.json(json)
-});
-
-router.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request recived`)
+        router.post('/api/notes', (req, res) => {
+            console.info(`${req.method} request recived`)
 
 
-    const { title, text } = req.body;
+            const { title, text } = req.body;
 
-    if (title && text) {
-        const newNote = {
-            title,
-            text,
-        }
+            if (title && text) {
+                const newNote = {
+                    title,
+                    text,
+                }
 
-        store
-        .saveNote(newNote)
-        .then(() => {
-            const response = {
-                status: 'success',
-                body: newNote,
-        }
-    })
+                store
+                    .saveNote(newNote)
+                    .then(() => {
+                        const response = {
+                            status: 'success',
+                            body: newNote,
+                        }
+                    })
 
-        console.log(response);
-        res.status(201).json(response);
-    } else {
-        res.status(500).json('Error creating post request')
-    }
-});
+                console.log(response);
+                res.status(201).json(response);
+            } else {
+                res.status(500).json('Error creating post request')
+            }
+        });
 
         const response = {
             status: 'success',
