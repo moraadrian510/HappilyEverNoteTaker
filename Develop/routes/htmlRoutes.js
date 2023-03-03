cf7ac322cf0545a597cbf37196d75763e0691707
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-03-02 21:43:36 -0800

    last commit

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -9,14 +9,11 @@ router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 })
 
-<<<<<<< HEAD
 // Get Route for homepage
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 })
 
-=======
->>>>>>> beb703d81ceeee5dcd8436655979c35ab637324c
 
 
 module.exports = router;
