dd3b8c441974542fe806c579611e71a9201a5d74
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-03-02 18:11:41 -0800

    added ppost /delete functions

--- Develop/db/store.js
+++ Develop/db/store.js
@@ -7,11 +7,11 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 class Store {
     read() {
-        return readFileAsync('db/db.json', 'utf8');
+        return readFileAsync('./db/db.json', 'utf8');
     }
 
     write(note) {
-        return writeFileAsync('db/db.json', JSON.stringify(note));
+        return writeFileAsync('./db/db.json', JSON.stringify(note));
     }
 
     async getNotes() {
@@ -28,6 +28,7 @@ class Store {
     async removeNote(id) {
         const notes = await this.getNotes();
         const filteredNotes = notes.filter(note => note.id !== id);
+        console.log(filteredNotes);
         await this.write(filteredNotes);
     }
 }
--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,8 +1,8 @@
 
 const router = require('express').Router();
 const store = require('../db/store');
-const json = require('../db/db.json');
-const fs = require('fs');
+const {v4: uuidv4} = require('uuid')
+// const fs = require('fs');
 
 
 // GET "/api/notes" responds with all notes from the database
@@ -17,7 +17,7 @@ router.get('/notes', (req, res) => {
 
 });
 
-router.post('/api/notes', (req, res) => {
+router.post('/notes', (req, res) => {
     console.info(`${req.method} request recived`)
 
 
@@ -27,58 +27,30 @@ router.post('/api/notes', (req, res) => {
         const newNote = {
             title,
             text,
+            id: uuidv4()
         }
-
-        // GET "/api/notes" responds with all notes from the database
-        router.get('/notes', (req, res) => {
-            console.log('route hit')
-            store
-                .getNotes()
-                .then((notes) => {
-                    return res.json(notes);
-                })
-                .catch((err) => res.status(500).json(err));
-            res.json(json)
-        });
-
-        router.post('/api/notes', (req, res) => {
-            console.info(`${req.method} request recived`)
-
-
-            const { title, text } = req.body;
-
-            if (title && text) {
-                const newNote = {
-                    title,
-                    text,
+        store
+            .addNote(newNote)
+            .then(() => {
+                const response = {
+                    status: 'success',
+                    body: newNote,
                 }
-
-                store
-                    .saveNote(newNote)
-                    .then(() => {
-                        const response = {
-                            status: 'success',
-                            body: newNote,
-                        }
-                    })
-
                 console.log(response);
                 res.status(201).json(response);
-            } else {
-                res.status(500).json('Error creating post request')
-            }
-        });
-
-        const response = {
-            status: 'success',
-            body: newNote,
-        };
-
-        console.log(response);
-        res.status(201).json(response);
+            })
+   
     } else {
         res.status(500).json('Error creating post request')
     }
+});
+
+router.delete('/notes/:id', (req, res) => {
+
+    const { id } = req.params
+    store.removeNote(id).then(() => {
+        res.status(200).end();
+    });
 });
 
 
